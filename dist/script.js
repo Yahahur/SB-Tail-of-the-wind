ae40a9a2db7f05ab12e8dbe7d18ec0c145823bcc
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2023-08-04 15:11:32 +0800

    push na muna

--- dist/script.js
+++ dist/script.js
@@ -1,27 +1,27 @@
-const kalboNav = document.querySelectorAll(".kalbo__nav__link");
-const kalboContent = document.querySelectorAll(".kalbo__item__content");
-kalboNav.forEach((kalbo) => {
-    kalbo.addEventListener("click", () => {
-        removeActiveKalbo();
-        kalbo.style = "transition:.5s ease";
-        kalbo.classList.add("active");
-        kalbo.style = "transition:.5s ease";
-        const activeContent = document.querySelector(`#${kalbo.id}-content`);
+const favoritesNav = document.querySelectorAll(".favorites__nav__link");
+const favoritesContent = document.querySelectorAll(".favorites__item__content");
+favoritesNav.forEach((favorites) => {
+    favorites.addEventListener("click", () => {
+        removeActivefavorites();
+        favorites.style = "transition:.5s ease";
+        favorites.classList.add("active");
+        favorites.style = "transition:.5s ease";
+        const activeContent = document.querySelector(`#${favorites.id}-content`);
         removeActiveContent();
         activeContent.classList.add("active");
     });
     
 });
 
-function removeActiveKalbo() {
-    kalboNav.forEach((kalbo) => {
-        kalbo.classList.remove("active");
+function removeActivefavorites() {
+    favoritesNav.forEach((favorites) => {
+        favorites.classList.remove("active");
     });
 }
 
 function removeActiveContent() {
-    kalboContent.forEach((kalbo) => {
-        kalbo.classList.remove("active");
+    favoritesContent.forEach((favorites) => {
+        favorites.classList.remove("active");
     });
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
             'lasalle':'#006341',
             'gento':'#006341',
             'semento':'hsl(240,1.51%,50.98%)',
+            'BG':'#d4e9e2',
+            'get':'#F1F8F5',
           }
     },
   },
