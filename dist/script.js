43c54c7d900d7d4719e69dc4461f2a65cd8c809e
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2023-08-04 14:26:00 +0800

    oow

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,36 @@
+const kalboNav = document.querySelectorAll(".kalbo__nav__link");
+const kalboContent = document.querySelectorAll(".kalbo__item__content");
+kalboNav.forEach((kalbo) => {
+    kalbo.addEventListener("click", () => {
+        removeActiveKalbo();
+        kalbo.style = "transition:.5s ease";
+        kalbo.classList.add("active");
+        kalbo.style = "transition:.5s ease";
+        const activeContent = document.querySelector(`#${kalbo.id}-content`);
+        removeActiveContent();
+        activeContent.classList.add("active");
+    });
+    
+});
+
+function removeActiveKalbo() {
+    kalboNav.forEach((kalbo) => {
+        kalbo.classList.remove("active");
+    });
+}
+
+function removeActiveContent() {
+    kalboContent.forEach((kalbo) => {
+        kalbo.classList.remove("active");
+    });
+}
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
 const hair = document.querySelector(".buhok");
 toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  headerNav.classList.toggle("open");
-  hair.classList.toggle("open");
+    toggleMenu.classList.toggle("open");
+    headerNav.classList.toggle("open");
+    hair.classList.toggle("open");
 });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
             'puti':'#ffffff',
             'maitim':'#2b2b2b',
             'lasalle':'#006341',
-            'semento':'hsl(240,1.51%,50.98%)'
+            'gento':'#006341',
+            'semento':'hsl(240,1.51%,50.98%)',
           }
     },
   },
