92c400db61217e4786987861839df9f6962ad684
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2023-08-07 15:48:31 +0800

    Drag

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
             'puti':'#ffffff',
             'maitim':'#2b2b2b',
             'lasalle':'#006341',
+            'medyo':'#D0EAE4',
             'gento':'#006341',
+            'maitimnagreen':'#013220',
             'semento':'hsl(240,1.51%,50.98%)',
             'BG':'#d4e9e2',
             'get':'#F1F8F5',
-          }
+          },
+          backgroundImage:{
+            'star':"url('./img/rewards/star.png')"
+          },
     },
   },
   plugins: [],
